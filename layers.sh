d06dce9412eca2b14c49d1064bf05433220ee721
Gabe Gonzalez <54686529+gabegon8910@users.noreply.github.com>
2023-04-08 13:44:45 -0400

    Update layers.sh
    Signed-off-by: Gabe Gonzalez <54686529+gabegon8910@users.noreply.github.com>

--- layers.sh
+++ layers.sh
@@ -13,18 +13,14 @@ mkdir $Lambda_Layer && cd $Lambda_Layer
 python3.8 -m venv python 
 
 source python/bin/activate 
-# while true; do
-#     read -p "Enter Python package name (leave blank to finish): " python_package
-#     if [[ -z "$python_package" ]]; then
-#         break  # exit the loop if user enters an empty string
-#     fi
-#     # check if the package exists before adding it to the list
-#     if ! pip3.8 show "$python_package" >/dev/null 2>&1; then
-#         echo "Error: package '$python_package' not found."
-#         continue  # skip this iteration of the loop and ask for another package
-#     fi
-#     python_packages+=("$python_package")  # add the package to the list
-# done
+while true; do
+    read -p "Enter Python package name (leave blank to finish): " python_package
+    if [[ -z "$python_package" ]]; then
+        break  # exit the loop if user enters an empty string
+    fi
+   
+    python_packages+=("$python_package")  # add the package to the list
+done
 
 # install all the packages in the list
 for package in "${python_packages[@]}"; do
