c5b5a3a47ed997ec76315b898a966872046daaed
Gabe Gonzalez <54686529+gabegon8910@users.noreply.github.com>
2023-04-08 11:58:51 -0400

    Create multiple_layers.sh

---
+++ multiple_layers.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+read -p "Enter Lambda layer name: " Lambda_Layer
+
+# initialize an empty list to store Python packages
+python_packages=()
+
+while true; do
+    read -p "Enter Python package name (leave blank to finish): " python_package
+    if [[ -z "$python_package" ]]; then
+        break  # exit the loop if user enters an empty string
+    fi
+    python_packages+=("$python_package")  # add the package to the list
+done
+
+sudo amazon-linux-extras enable python3.8
+
+sudo yum install python38 -y 
+
+mkdir $Lambda_Layer && cd $Lambda_Layer
+
+python3.8 -m venv python 
+
+source python/bin/activate 
+
+# install all the packages in the list
+for package in "${python_packages[@]}"; do
+    pip3.8 install "$package"
+done
+
+deactivate
+
+zip -r $Lambda_Layer.zip python/lib
