9cfe2cfafd2f0c5453e67774c02c80b2d2438411
Gabe <gabe@gonzalezs.net>
2023-08-22 15:00:49 -0400

    added version to allow user to select the python runtime enviroment

--- layers.sh
+++ layers.sh
@@ -5,14 +5,16 @@ read -p "Enter Lambda layer name: " Lambda_Layer
 read -p "Enter Python runtime version (e.g., 3.10): " Python_Version
 
 # Check if the specified Python version is already installed
-if command -v python${Python_Version} &>/dev/null; then
-    echo "Python ${Python_Version} is already installed."
+if command -v python3.${Python_Version} &>/dev/null; then
+    echo "Python 3.${Python_Version} is already installed."
 else
-    echo "Python ${Python_Version} is not installed. Installing..."
-    
-    # Install the specified Python runtime
-    sudo amazon-linux-extras enable python${Python_Version}
-    sudo yum install python${Python_Version} -y
+    echo "Python 3.${Python_Version} is not installed. Installing..."
+
+    # Enable the Amazon Linux Extras repository for Python 3.10
+    sudo amazon-linux-extras enable python3.10
+
+    # Install Python 3.10
+    sudo yum install python3.10 -y
 fi
 
 # Initialize an empty list to store Python packages
@@ -22,7 +24,7 @@ python_packages=()
 mkdir $Lambda_Layer && cd $Lambda_Layer
 
 # Create a virtual environment for the specified Python version
-python${Python_Version} -m venv python
+python3.${Python_Version} -m venv python
 
 # Activate the virtual environment
 source python/bin/activate
@@ -38,7 +40,7 @@ done
 
 # Install all the packages in the list
 for package in "${python_packages[@]}"; do
-    pip${Python_Version} install "$package"
+    pip3.${Python_Version} install "$package"
 done
 
 # Deactivate the virtual environment
